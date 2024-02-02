49994e269f35dd01b3c42cb563453a6bf439457d
Chris Bilger <christopherbilg@gmail.com>
2024-02-02 22:58:46 +0000

    Test

--- migrations/0000_sad_diamondback.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `slideshow` (
-	`id` text PRIMARY KEY NOT NULL,
-	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`deleted_at` integer
-);
