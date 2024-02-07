d5a67be3f2c4aa622272e35d8611381f6a7c1374
Chris Bilger <christopherbilg@gmail.com>
2024-02-07 00:37:32 +0000

    test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
+import NeonDB from "~/db/client.server";
+import { UserTable } from "~/db/schema";
 import { getSession } from "~/sessions";
 
 export const meta: MetaFunction = () => {
@@ -9,12 +11,15 @@ export const meta: MetaFunction = () => {
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
 
+  const x = await NeonDB.getInstance().db.select().from(UserTable);
+
   if (!session.has("id")) return null;
 
   return {
     id: session.get("id"),
     firstName: session.get("firstName"),
     lastName: session.get("lastName"),
+    length: x.length,
   };
 };
 
@@ -25,6 +30,10 @@ const LandingRoute = () => {
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col space-y-4">
         <h1 className="text-4xl font-bold">Welcome to Jittter!</h1>
+
+        <hr />
+
+        <p>{length}</p>
 
         <hr />
 
