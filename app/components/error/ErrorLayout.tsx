61e49a9022076d94695a7efa565cbcadac4f40c5
Chris Bilger <christopherbilg@gmail.com>
2024-02-15 16:03:19 +0000

    feat: 4xx and 5xx RouteError pages added

---
+++ app/components/error/ErrorLayout.tsx
@@ -0,0 +1,42 @@
+import { ErrorResponse } from "@remix-run/cloudflare";
+import { Link } from "@remix-run/react";
+
+type ErrorLayoutProps = {
+  error: ErrorResponse;
+};
+
+const ErrorLayout = ({ error }: ErrorLayoutProps) => {
+  return (
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-blue-600">{error.status}</p>
+
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          {error.statusText}
+        </h1>
+
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          {error.status === 404 &&
+            "Sorry, we couldn't find the page you're looking for."}
+          {error.status === 500 &&
+            "Sorry, we're having technical difficulties. Please try again later."}
+        </p>
+
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            Go back home
+          </Link>
+
+          {/* <a href="#_" className="text-sm font-semibold text-gray-900">
+            Contact support <span aria-hidden="true">&rarr;</span>
+          </a> */}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ErrorLayout;
