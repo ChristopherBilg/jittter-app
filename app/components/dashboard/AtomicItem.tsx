764bd56e2d98abe840bdc15ee2a32ca0d531d978
Chris Bilger <christopherbilg@gmail.com>
2024-02-23 00:27:07 +0000

    Todo 2 done

--- app/components/dashboard/AtomicItem.tsx
+++ app/components/dashboard/AtomicItem.tsx
@@ -12,13 +12,11 @@ type AtomicItemProps = {
 
 const AtomicItem = ({ atom }: AtomicItemProps) => {
   switch (atom.type) {
-    case "note": {
+    case "note":
       return <AtomicNote atom={atom as AtomStructure<NoteStructure>} />;
-    }
     case "contact":
       return <AtomicContact atom={atom as AtomStructure<ContactStructure>} />;
     default:
-      // TODO: Render a default component for unknown atom types (e.g. AtomicUnknown) with a way to delete the atom
       return null;
   }
 };
